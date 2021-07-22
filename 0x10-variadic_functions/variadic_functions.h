965cf678c28ea1b93d290036446058ffe5d9fb7b
Selamu Tadesse <selamut@gmail.com>
2021-07-22 06:37:48 -0700

    variadic functions files added

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,24 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+  char *op;
+  void (*f)(va_list pr);
+} op_t;
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
